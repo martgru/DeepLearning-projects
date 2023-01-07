817c956e432d3ac5f267b59fc2ec3e48be323c62
Martyna Gruszka <47233360+martgru@users.noreply.github.com>
2023-01-07 09:09:40 +0900

    Create useful_functions_eval.py
    Useful functions for plotting and comparing TensorFlow history objects

---
+++ useful_functions_eval.py
@@ -0,0 +1,36 @@
+def compare_histories(history_before, history_after, initial_epochs):
+  """
+    Compares two tensorflow History objects.
+  """
+
+  # original
+  acc = history_before.history["accuracy"]
+  loss = history_before.history["loss"] 
+  val_acc = history_before.history["val_accuracy"]
+  val_loss = history_before.history["val_loss"]
+
+  # after fine-tuning
+  total_acc = acc + history_after.history["accuracy"]
+  total_loss = loss + history_after.history["loss"] 
+  total_val_acc = val_acc + history_after.history["val_accuracy"]
+  total_val_loss = val_loss + history_after.history["val_loss"]
+
+  # plot
+  plt.figure(figsize=(8,10))
+  
+
+  plt.subplot(2,1,1)
+  plt.title("Accuracy curve")
+  plt.plot(total_acc, label="acc")
+  plt.plot(total_val_acc, label="val_acc")
+  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label="Start fine-tuning")
+  plt.xlabel("Epochs")
+  plt.legend(loc="lower right")
+
+  plt.subplot(2,1,2)
+  plt.title("Loss curve")
+  plt.plot(total_loss, label="loss")
+  plt.plot(total_val_loss, label="val_loss")
+  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label="Start fine-tuning")
+  plt.xlabel("Epochs")
+  plt.legend()
