e3bb8effca158895226bdb9eb0568e796d326c3a
Martyna Gruszka <47233360+martgru@users.noreply.github.com>
2023-01-16 14:49:46 +0900

    Update useful_functions_eval.py
    plot_loss_curves function

--- useful_functions_eval.py
+++ useful_functions_eval.py
@@ -36,3 +36,32 @@ def compare_histories(history_before, history_after, initial_epochs):
   plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label="Start fine-tuning")
   plt.xlabel("Epochs")
   plt.legend()
+  
+  
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics.
+  
+  """ 
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
+
+  epochs = range(len(history.history['loss']))
+
+  # Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend();
